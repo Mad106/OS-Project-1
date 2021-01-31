5ecb3336ccd77bbf3c1edbdd6dce472423e83e3c
Jessica Mitchell <42589541+MITCHELLNJESS@users.noreply.github.com>
2021-01-30 20:57:08 -0500

    added structs to account for job functions

--- shell.h
+++ shell.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #ifndef _SHELL_H
 #define _SHELL_H
@@ -11,11 +13,29 @@
  * C files, allowing the functions to be called between each
  * one without errors */
 
+typedef struct {
+	int size;
+	char **items;
+} tokenlist;
+
+typedef struct {
+    int jobId;
+    pid_t pid;
+    tokenlist* tokens;
+} bgjob_t;
+
+typedef struct {
+    int size;
+    bgjob_t** jobs;
+} bgjobslist;
+
 void Echo(int, char**);
 void Prompt();
 void Parse();
 char * Tilde();
 const char* EVar(const char *name);
-void Path(char**);
+void Path(tokenlist *tokens, bgjobslist* jobs);
+/* Background jobs function */
+void Jobs(bgjobslist* jobs, int showAll);
 
 #endif
